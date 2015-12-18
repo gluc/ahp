c2fb36c819a4bc91dca432c40920c89c5fc5df5c
Christoph Glur <christoph.glur@ipub.ch>
2015-12-18 07:19:08 +0100

    started to clean up (still failing)

--- R/ahp.package.R
+++ R/ahp.package.R
@@ -1,7 +1,7 @@
 #' ahp AHP (Analytic Hierarchy Process) Modeling for R
 #' 
 #' AHP (Analytic Hierarchy Process) is a decision making framework developed by Thomas Saaty.
-#' This package lets you model complex decision problems according to the AHP framework.  
+#' This package lets you model and analyse complex decision making problems according to the AHP framework.  
 #'
 #' @docType package
 #' @name ahp
