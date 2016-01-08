21d84e862084d6599151c67cb3d930cd3e4636b8
gluc <christoph.glur@ipub.com>
2016-01-08 08:32:15 +0000

    file-format now suitable for shiny

---
+++ tests/testthat/test-Visualize.R
@@ -0,0 +1,15 @@
+context("ahp visualize")
+
+
+test_that("Visualize", {
+  ahpFile <- system.file("extdata", "car.ahp", package="ahp")
+  carAhp <- Load(ahpFile)
+  Calculate(carAhp)
+  graph <- GetGraph(carAhp)
+  expect_equal(class(graph), "dgr_graph")
+  expect_equal(dim(graph$nodes_df), c(17, 4))
+  
+})
+
+
+
