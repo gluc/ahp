86f544c1efc87b4087b99ba394d2db4459d46e0b
gluc <christoph.glur@ipub.com>
2017-01-06 12:56:43 +0000

    fixed tests

--- tests/testthat/test-Visualize.R
+++ tests/testthat/test-Visualize.R
@@ -8,9 +8,9 @@ test_that("Visualize", {
   graph <- GetGraph(carAhp)
   expect_equal(class(graph), "dgr_graph")
   expect_equal(dim(graph$nodes_df), c(18, 10))
-  expect_equal(names(graph$nodes_df), c("nodes", "type", "label", "tooltip", "style", "shape", "color", "fillcolor", "penwidth", "fontname"))
-  expect_equal(dim(graph$edges_df), c(24, 7))
-  expect_equal(names(graph$edges_df), c("from", "to", "arrowhead", "color", "penwidth", "rel", "dir"))
+  expect_equal(names(graph$nodes_df), c("id", "type", "label", "tooltip", "style", "shape", "color", "fillcolor", "penwidth", "fontname"))
+  expect_equal(dim(graph$edges_df), c(24, 8))
+  expect_equal(names(graph$edges_df), c("id", "from", "to", "rel", "arrowhead", "color", "penwidth", "dir"))
   
 })
 
